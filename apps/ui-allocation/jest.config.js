d67c4e51b187f565e4d85bd79548839d58ffd2be
Steve Buonincontri <steve@soaconsultingservices.com>
2021-09-29 18:41:19 -0400

    new

---
+++ apps/ui-allocation/jest.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  displayName: 'ui-allocation',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: {
+        before: ['jest-preset-angular/build/InlineFilesTransformer', 'jest-preset-angular/build/StripStylesTransformer']
+      }
+    }
+  },
+  coverageDirectory: '../../coverage/apps/ui-allocation',
+  coverageThreshold: {
+    global: {
+      statement: 100,
+      branches: 100,
+      lines: 100,      
+      functions: 100
+    }
+  },  
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
+  ]
+};
